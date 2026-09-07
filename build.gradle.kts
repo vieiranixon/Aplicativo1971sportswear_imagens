743eb7c616ff0c8328f88c41b4b60ad2da54f700
vieiranixon <vieiranixon@gmail.com>
2026-09-07 18:20:37 -0300

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,22 @@
 plugins {
-    id("com.android.application") version "8.6.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+android {
+    namespace = "com.nvn.baixador"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.nvn.baixador"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 3
+        versionName = "3.0"
+    }
+}
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("org.jsoup:jsoup:1.18.3")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
